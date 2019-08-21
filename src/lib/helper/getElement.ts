4e894ecdcee8e1d4c63cba9bcc5b7abe45ef2fec
MohamedElmdary <engm5081@gmail.com>
2019-08-21 04:09:14 +0200

    getElement

---
+++ src/lib/helper/getElement.ts
@@ -0,0 +1,19 @@
+export function getElement<T = Element>(
+  selector: string,
+  method: (selector: string) => any,
+  key: string,
+  toArr: boolean = false
+): T {
+  if (typeof selector !== 'string') {
+    throw new Error(`'${selector}' is not assign to ${key}<string>.`);
+  }
+  const el = method(selector);
+  if (!el) {
+    throw new Error(`No Element with ${key}: '${selector}' was found.`);
+  }
+  if (toArr) {
+    // @ts-ignore
+    return Array.from(el) as T;
+  }
+  return el as T;
+}
