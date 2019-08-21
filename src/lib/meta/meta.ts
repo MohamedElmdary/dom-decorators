578fa4db5ac9b9ce282bfdfb1bae90a1bcdfd35d
MohamedElmdary <engm5081@gmail.com>
2019-08-21 04:42:27 +0200

    added global meta container

--- src/lib/meta/meta.ts
+++ src/lib/meta/meta.ts
@@ -1,26 +1,61 @@
 'use strict';
 
 export const DOCUMENT = '__@DOCUMENT__';
+export const GLOBAL = '__@GLOBAL__';
 
-export const META: { [key: string]: any } = {};
+export const META: {
+  [GLOBAL]: { [key: string]: any };
+  [key: string]: { [key: string]: any };
+} = {
+  [GLOBAL]: {}
+};
 
 export function registerMeta<TMetaResolver = any>(
+  key: string | symbol,
+  value: TMetaResolver,
+  method: string
+) {
+  key = key.toString();
+  if (!META[method]) {
+    META[method] = {};
+  }
+  if (META[method][key]) {
+    throw new Error(`'${key}' meta is already defined on method: ${method}()`);
+  }
+  META[method][key] = value as TMetaResolver;
+}
+
+export function registerGlobalMeta<TMetaResolver = any>(
   key: string | symbol,
   value: TMetaResolver
 ) {
   key = key.toString();
-  if (META[key]) {
-    throw new Error(`'${key}' meta is already defined!`);
+  if (META[GLOBAL][key]) {
+    throw new Error(`'${key}' meta is already globaly defined!`);
   }
-  META[key] = value as TMetaResolver;
+  META[GLOBAL][key] = value as TMetaResolver;
 }
 
 export function resolveMeta<TMetaResolver = any>(
+  key: string | symbol,
+  method: string
+): TMetaResolver {
+  key = key.toString();
+  if (!META[method]) {
+    throw new Error(`${method}() has no defined any meta!`);
+  }
+  if (!META[method][key]) {
+    throw new Error(`${key} meta was never registerd for ${method}()`);
+  }
+  return META[method][key] as TMetaResolver;
+}
+
+export function resolveGlobalMeta<TMetaResolver = any>(
   key: string | symbol
 ): TMetaResolver {
   key = key.toString();
-  if (!META[key]) {
-    throw new Error(`${key} meta was never registerd!`);
+  if (!META[GLOBAL][key]) {
+    throw new Error(`${key} meta was never defined!`);
   }
-  return META[key] as TMetaResolver;
+  return META[GLOBAL][key] as TMetaResolver;
 }
